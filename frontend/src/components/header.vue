a4aae4f6f7590cb457751b8bba97fdec911edbe2
Pavlo Getta <pavlo.getta@epfl.ch>
2018-07-11 11:14:32 +0200

    Frontend: show circuit name in the header
    Change-Id: Ia9656e3a7b1325cf76a2eb9ed59b11f33cd082c9

--- frontend/src/components/header.vue
+++ frontend/src/components/header.vue
@@ -2,14 +2,19 @@
 <template>
   <header>
     <div class="title">
-      Subcellular app
+      Subcellular app: {{ circuitName }}
     </div>
   </header>
 </template>
 
 
 <script>
+  const circuitName = process.env.VUE_APP_CIRCUIT_NAME;
+
   export default {
     name: 'header-component',
+    data() {
+      return { circuitName };
+    },
   };
 </script>
