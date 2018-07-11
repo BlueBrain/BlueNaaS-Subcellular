401e2c95b1fd749af3e9ca34308a3b5722dc7301
Pavlo Getta <pavlo.getta@epfl.ch>
2018-07-11 11:00:17 +0200

    Frontend: init iView UI with en-US locale
    Change-Id: I2876e5019c7fb0f133d98f9a76208cf8441573d7

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,10 +3,11 @@ import 'iview/dist/styles/iview.css';
 
 import Vue from 'vue';
 import iView from 'iview';
+import locale from 'iview/dist/locale/en-US';
 
 import App from './App.vue';
 
-Vue.use(iView);
+Vue.use(iView, { locale });
 
 Vue.config.productionTip = true;
 
