2f9ada1b1000f35dad2164a5eba8b3300c49ee8e
Pavlo Getta <pavlo.getta@epfl.ch>
2019-06-27 13:09:03 +0200

    Frontend: add user profile
    Change-Id: I4698763c6e5eb9d2ffe96fb23a9cb821e0b6133d

--- frontend/src/components/header.vue
+++ frontend/src/components/header.vue
@@ -2,19 +2,64 @@
 <template>
   <header>
     <div class="title">
-      Subcellular app: {{ circuitName }}
+      <span>Subcellular app. </span>
+      <span v-if="modelName"> Model: {{ modelName }}</span>
     </div>
+    <div
+      class="user-profile"
+      @click="showUserProfile"
+    >
+      {{ user.fullName || 'Unknown user' }}
+      <Icon
+        class="user-icon"
+        type="md-person"
+      />
+    </div>
+
+    <user-profile-drawer v-model="userProfileVisible"/>
   </header>
 </template>
 
 
 <script>
-  const circuitName = process.env.VUE_APP_CIRCUIT_NAME;
+  import UserProfileDrawer from '@/components/shared/user-profile-drawer.vue';
 
   export default {
     name: 'header-component',
+    components: {
+      'user-profile-drawer': UserProfileDrawer,
+    },
     data() {
-      return { circuitName };
+      return {
+        userProfileVisible: false,
+      };
+    },
+    methods: {
+      showUserProfile() {
+        this.userProfileVisible = true;
+      },
+    },
+    computed: {
+      modelName() {
+        return this.$store.state.model.name;
+      },
+      user() {
+        return this.$store.state.user;
+      },
     },
   };
 </script>
+
+
+<style lang="scss" scoped>
+  .title {
+    display: inline-block;
+  }
+  .user-profile {
+    float: right;
+    cursor: pointer;
+  }
+  .user-icon {
+    line-height: 28px;
+  }
+</style>
---
+++ frontend/src/components/shared/user-profile-drawer.vue
@@ -0,0 +1,109 @@
+
+<template>
+  <Drawer
+    title="User profile"
+    v-model="userProfileVisible"
+    :styles="userProfileDrawerStyle"
+    width="400"
+    @input="onUserProfileVisibilityChange"
+  >
+    <i-form :label-width="70">
+      <FormItem label="Full name">
+        <i-input v-model="tmpUser.fullName" />
+      </FormItem>
+      <FormItem label="Email">
+        <i-input v-model="tmpUser.email" />
+      </FormItem>
+      <FormItem label="User id">
+        <i-input v-model="user.id" readonly />
+      </FormItem>
+    </i-form>
+
+    <div class="userprofile-drawer-footer">
+      <i-button
+        class="mr-12"
+        @click="hideUserProfile"
+      >
+        Cancel
+      </i-button>
+      <i-button
+        type="primary"
+        :disabled="!saveBtnActive"
+        @click="onSaveUserProfileBtnClick"
+      >
+        Ok
+      </i-button>
+    </div>
+  </Drawer>
+</template>
+
+
+<script>
+  import pick from 'lodash/pick';
+
+  import isEqualBy from '@/tools/is-equal-by';
+
+  const userProfileDrawerStyle = {
+    height: 'calc(100% - 28px)',
+    overflow: 'auto',
+    paddingBottom: '28px',
+    position: 'static',
+    color: 'red',
+  };
+
+  export default {
+    name: 'user-profile-drawer',
+    props: ['value'],
+    data() {
+      return {
+        userProfileDrawerStyle,
+        tmpUser: {
+          fullName: null,
+          email: null,
+        },
+        userProfileVisible: false,
+      };
+    },
+    methods: {
+      onSaveUserProfileBtnClick() {
+        this.$store.dispatch('setUser', Object.assign({}, this.user, this.tmpUser));
+        this.hideUserProfile();
+      },
+      hideUserProfile() {
+        this.userProfileVisible = false;
+        this.onUserProfileVisibilityChange();
+      },
+      onUserProfileVisibilityChange() {
+        this.$emit('input', false);
+      },
+    },
+    computed: {
+      user() {
+        return this.$store.state.user;
+      },
+      saveBtnActive() {
+        return !isEqualBy(this.user, this.tmpUser, ['fullName', 'email']);
+      },
+    },
+    watch: {
+      value() {
+        this.userProfileVisible = this.value;
+        this.tmpUser = pick(this.user, ['fullName', 'email']);
+      },
+    },
+  };
+</script>
+
+
+<style lang="scss" scoped>
+  .userprofile-drawer-footer {
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    border-top: 1px solid #e8e8e8;
+    padding: 10px 16px;
+    text-align: right;
+    background: #fff;
+  }
+</style>
