a9cad9a20e19aa7f5cb94e50174a3b1beb3e0b02
Gil Arturo Barrios Del Villar <gil.barriosdelvillar@epfl.ch>
2021-10-28 22:23:16 +0200

    url
    Change-Id: I084bfd73e39856108a4fe4c1549912bf1002367b

--- frontend/src/store/actions.ts
+++ frontend/src/store/actions.ts
@@ -73,7 +73,10 @@ export default {
     const currentModel = { ...state.model };
     currentModel.userId = state.user.id;
 
-    const res = await axios.post('http://127.0.0.1:8888/models', currentModel);
+    const res = await axios.post(
+      'https://subcellular-bsp-epfl.apps.hbp.eu/api/models',
+      currentModel,
+    );
 
     currentModel.id = res.data.id;
 
