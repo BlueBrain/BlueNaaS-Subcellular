8f5626536e93bad648ee0ecf97f210893eafb25c
Gil Arturo Barrios Del Villar <gil.barriosdelvillar@epfl.ch>
2021-12-20 15:17:52 +0100

    deploy
    Change-Id: Ib8c9fbce9837a543ee0270ef281dd4adc82d2826

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
-make docker_build_latest
+VUE_APP_SENTRY_DSN=https://957bc91f672d4e8a83f09af8ad015a6e@sentry.io/1374434
+yarn lint && yarn build
+docker build -t sc .
 docker tag sc docker-registry.ebrains.eu/bsp-epfl/subcellular
 docker push docker-registry.ebrains.eu/bsp-epfl/subcellular
 oc import-image docker-registry.ebrains.eu/bsp-epfl/subcellular
