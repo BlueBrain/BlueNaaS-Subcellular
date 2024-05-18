04a218b7be5edecf1f32c332c2390878b0908b3d
Gil Arturo Barrios Del Villar <gil.barriosdelvillar@epfl.ch>
2024-05-18 11:22:57 +0200

    log api request

--- backend/subcellular_experiment/api.py
+++ backend/subcellular_experiment/api.py
@@ -1,12 +1,18 @@
 import requests
 import os
+from .logger import get_logger
 
 URL = os.getenv("API_URL")
 # URL = "http://api:8001"
 
+L = get_logger(__name__)
+L.debug('URL')
 
 def fetch_model(model_id: str, user_id: str):
     model_r = requests.get(f"{URL}/model-detail/{model_id}?user_id={user_id}")
+    L.debug(model_r.status_code)
+    L.debug(model_r.json())
+
 
     if model_r.status_code != 200:
         raise ConnectionError("Can't reach api")
